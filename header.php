855e614c93849d8d3c5014b747056d953c89ddb6
webmvw <masudrana.bbpi@gmail.com>
2021-06-01 16:30:24 +0600

    separate template header and footer

---
+++ header.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <!-- Title -->
+      <title>Halim | Onepage Multipurpose Website</title>
+      <!-- Font Google -->
+      <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
+      <!-- Bootstrap Css -->
+      <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+      <!-- Font Awesome Css -->
+      <link href="assets/css/font-awesome.min.css" rel="stylesheet">
+      <!-- Magnific Popup Css -->
+      <link href="assets/css/magnific-popup.css" rel="stylesheet">
+      <!-- Owl Carousel Css -->
+      <link href="assets/css/owl.carousel.css" rel="stylesheet">
+      <!-- Main Css -->
+      <link href="assets/css/style.css" rel="stylesheet">
+      <!-- Responsive Css -->
+      <link href="assets/css/responsive.css" rel="stylesheet">
+
+      <?php wp_head(); ?>
+
+   </head>
+   <body>
+	   <section class="header-top">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 col-sm-12">
+						<div class="header-left">
+							<a href=""><i class="fa fa-envelope"></i> info@halim.com</a>
+							<a href=""><i class="fa fa-phone"></i> 23457689</a>
+						</div>
+					</div>
+					<div class="col-md-6 col-sm-12 text-right">
+						<div class="header-social">
+							<a href=""><i class="fa fa-facebook"></i></a>
+							<a href=""><i class="fa fa-twitter"></i></a>
+							<a href=""><i class="fa fa-youtube"></i></a>
+							<a href=""><i class="fa fa-linkedin"></i></a>
+							<a href=""><i class="fa fa-google-plus"></i></a>
+						</div>
+					</div>
+				</div>
+			</div>
+	   </section>
+      <!-- Header Area Start -->
+      <header class="header header-fixed">
+         <div class="container">
+            <div class="row">
+               <div class="col-xl-12">
+                  <nav class="navbar navbar-expand-md navbar-light">
+                     <a class="navbar-brand" href="#">halim</a>
+                     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                     <span class="navbar-toggler-icon"></span>
+                     </button>
+                     <div class="collapse navbar-collapse ml-auto mainmenu" id="navbarNav">
+                        <ul class="navbar-nav ml-auto">
+                           <li><a href="index.html">Home</a></li>
+                           <li><a href="about.html">About</a></li>
+                           <li><a href="services.html">Services</a></li>
+                           <li><a href="portfolio.html">Portfolio</a></li>
+                           <li><a href="gallery.html">Gallery</a></li>
+                           <li><a href="blog.html">Blog</a></li>
+                           <li><a href="contact.html">Contact</a></li>
+                        </ul>
+                     </div>
+                  </nav>
+               </div>
+            </div>
+         </div>
+      </header>
+      <!-- Header Area Start -->
